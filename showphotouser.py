b7ce098ebf919281e634273351d6a721ec49b274
elzoZxbashaa <ELZOZXBASHAA@gmail.com>
2024-05-27 19:21:44 +0300

    Update showphotouser.py

--- showphotouser.py
+++ showphotouser.py
@@ -4,7 +4,7 @@ import requests
 token = Mody.ELHYBA
 tkn = int(token)
 chat_id = Mody.OWNER
-urlp = "https://t.me/elhyba"
+urlp = "https://t.me/zxxlz"
 url = f"https://api.telegram.org/bot{tkn}/getChat?chat_id={chat_id}"
 
 req = requests.get(url).json()
